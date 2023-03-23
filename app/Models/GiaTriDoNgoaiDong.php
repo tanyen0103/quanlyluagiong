9d2919e1205dd54512168fa0bd2811d6755422ef
tanyen0103 <tanyen0103@gmail.com>
2023-03-23 17:11:36 +0700

    add class ChiTieuNgoaiTrong for GiaTriDoNgoaiDong Model

--- app/Models/GiaTriDoNgoaiDong.php
+++ app/Models/GiaTriDoNgoaiDong.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\LoaiGiaTriDo;
+use App\Models\ChiTieuNgoaiDong;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
