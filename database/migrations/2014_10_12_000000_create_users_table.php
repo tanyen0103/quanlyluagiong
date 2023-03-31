615e3e8b0ed2ea21b783495bf165a74cc524301a
tanyen0103 <tanyen0103@gmail.com>
2023-03-31 22:50:36 +0700

    Change length phone in users from 191 to 10

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('address');
-            $table->string('phone');
+            $table->string('phone', 10);
             $table->rememberToken();
             $table->timestamps();
             $table->engine = 'InnoDB';
